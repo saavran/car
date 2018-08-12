20b116093de0e2ab0c9535cca053949fa0a6a878
Sarvagya Saavran <s.saavran@gmail.com>
2018-08-12 22:42:36 +0530

    New Animations

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var image = document.getElementById("ImageOne");
 var graphImage = document.getElementById("ImageTwo");
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var box4 = document.getElementById("box4");
+var box5 = document.getElementById("box5");
+var needle = document.getElementById("iconMeter");
 
 function changeRed()
-{
-    
+{   
+    box1.style.width = "180px";
+    box2.style.width = "300px";
+    box3.style.width = "280px";
+    box4.style.width = "210px";
+    box5.style.width = "190px";
+    box1.style.backgroundColor = "red";
+    box2.style.backgroundColor = "red";
+    box3.style.backgroundColor = "red";
+    box4.style.backgroundColor = "red";
+    box5.style.backgroundColor = "red";
+    needle.style.transform = "rotate(70deg)";
     if (image.getAttribute('src'))
     {
         image.src = "red.jpg"
@@ -12,7 +28,17 @@ function changeRed()
 }
 function changeBlue()
 {
-    
+    box1.style.width = "300px";
+    box2.style.width = "280px";
+    box3.style.width = "320px";
+    box4.style.width = "240px";
+    box5.style.width = "200px";
+    box1.style.backgroundColor = "blue";
+    box2.style.backgroundColor = "blue";
+    box3.style.backgroundColor = "blue";
+    box4.style.backgroundColor = "blue";
+    box5.style.backgroundColor = "blue";  
+    needle.style.transform = "rotate(110deg)"; 
     if (image.getAttribute('src'))
     {
         image.src = "blue.jpg"
@@ -20,8 +46,18 @@ function changeBlue()
     }
 }
 function changeGrey()
-{
-    
+{   
+    box1.style.width = "280px";
+    box2.style.width = "160px";
+    box3.style.width = "220px";
+    box4.style.width = "300px";
+    box5.style.width = "230px";
+    box1.style.backgroundColor = "grey";
+    box2.style.backgroundColor = "grey";
+    box3.style.backgroundColor = "grey";
+    box4.style.backgroundColor = "grey";
+    box5.style.backgroundColor = "grey";
+    needle.style.transform = "rotate(100deg)"; 
     if (image.getAttribute('src'))
     {
         image.src = "grey.jpg"
@@ -30,7 +66,17 @@ function changeGrey()
 }
 function changeSilver()
 {
-    
+    box1.style.width = "230px";
+    box2.style.width = "320px";
+    box3.style.width = "290px";
+    box4.style.width = "210px";
+    box5.style.width = "300px";
+    box1.style.backgroundColor = "silver";
+    box2.style.backgroundColor = "silver";
+    box3.style.backgroundColor = "silver";
+    box4.style.backgroundColor = "silver";
+    box5.style.backgroundColor = "silver";
+    needle.style.transform = "rotate(90deg)"; 
     if (image.getAttribute('src'))
     {
         image.src = "silver.jpg"
